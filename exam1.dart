e1f86ff67d3bc050d525f5330a658f7eb1f8b96a
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-06-24 23:15:11 +0600

    First Exam 1

---
+++ exam1.dart
@@ -0,0 +1,24 @@
+class Media {
+  void play() {
+    print('Playing media...');
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
+
+void main() {
+  Media mediaInstance = Media();
+  Song songInstance = Song('The Beatles');
+
+  mediaInstance.play();
+  songInstance.play();
+}
